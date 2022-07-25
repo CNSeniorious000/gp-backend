09dd3ed826d3048a76abafa618b5c9a64602ba52
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-07-25 18:06:50 +0800

    add top-level package to avoid ImportError: attempted relative import beyond top-level package

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from starlette.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from brotli_asgi import BrotliMiddleware
+from core.user import router as user_router
 from starlette.requests import Request
-from user import router as user_router
 from fastapi import FastAPI
 from os import system
 
