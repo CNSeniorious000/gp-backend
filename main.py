a282c7c2be9a01b51813dc0dc1f2817a17f6bd6b
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-08-10 18:48:50 +0800

    use iframe redirect

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from starlette.responses import RedirectResponse, HTMLResponse
 from starlette.templating import Jinja2Templates
-from starlette.responses import RedirectResponse
-from brotli_asgi import BrotliMiddleware
+from starlette.staticfiles import StaticFiles
 from core.user import router as user_router
+from brotli_asgi import BrotliMiddleware
 from starlette.requests import Request
+from httpx import AsyncClient
 from fastapi import FastAPI
 from os import system
 
-app = FastAPI(title="Guard Pine")
+app = FastAPI(title="守护青松 Guard Pine")
 app.add_middleware(BrotliMiddleware, quality=11, minimum_size=256)
 app.include_router(user_router)
 
@@ -30,6 +32,27 @@ async def index_page(request: Request):
 
 @app.get("/refresh")
 def git_pull():
-    """trigger a git pull command in the terminal"""
+    """trigger a git pull command in local terminal and redirect to document page"""
     system("git pull")
-    return RedirectResponse("/")
+    return RedirectResponse("/docs")
+
+
+client = AsyncClient(http2=True)
+
+
+async def get_iframe(url, title=None) -> bytes:
+    return (await client.get("http://localhost:7777/link", params={"url": url, "title": title})).content
+
+
+@app.get("/notes", response_class=HTMLResponse)
+async def get_notes():
+    return await get_iframe("https://www.craft.do/s/bbKnMXuNDudwop", "守护青松 - 开发者说")
+
+
+@app.get("/apifox", response_class=HTMLResponse)
+async def get_apidoc():
+    return await get_iframe("https://www.apifox.cn/apidoc/project-1338127", "守护青松 - 接口文档")
+
+
+app.mount("/static", StaticFiles(directory="static"))
+app.mount("/", StaticFiles(directory="static"))
