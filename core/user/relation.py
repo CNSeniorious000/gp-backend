f5d8efa3f4d45dff2ae1d2bcc5cc275e353cb6bf
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-09-27 20:36:11 +0800

    modify relative APIs

--- core/user/relation.py
+++ core/user/relation.py
@@ -13,30 +13,33 @@ class RelationItem(SQLModel, table=True):
     relation: str
 
 
-class RelationPut(BaseModel):
-    from_user_id: str
-    to_user_id: str
-    relation: str
-    permission: bool = False
+class RelativePut(BaseModel):
+    from_user_id: str | None = Field(None, description="添加谁的亲戚")
+    to_user_id: str = Field("id", description="添加谁")
+    relation: str = Field("父/母", description="什么关系")
 
 
-@router.put("/relative")
-async def add_relative(data: RelationPut, bearer: Bearer = Depends()):
-    ensure(data.from_user_id), ensure(data.to_user_id)
-    assert verify_permitted(bearer.id, data.from_user_id)
+@router.put("/relative", response_model=RelationItem)
+async def add_relative(data: RelativePut, bearer: Bearer = Depends()):
+    from_user_id = bearer.id if data.from_user_id is None else ensure(data.from_user_id)
+    to_user_id = ensure(data.to_user_id)
+
+    if bearer.id != from_user_id:
+        assert verify_permitted(bearer.id, from_user_id)
 
     with Session(engine) as session:
         session.add(item := RelationItem(
-            from_user_id=data.from_user_id, to_user_id=data.to_user_id, relation=data.relation
+            from_user_id=from_user_id, to_user_id=to_user_id, relation=data.relation
         ))
         session.commit()
+        session.refresh(item)
 
     return item
 
 
 @router.get("/relative")
-async def get_relatives(id: str | None = None, bearer: Bearer = Depends()):
-    from_user_id = id or bearer.id
+async def get_relatives(id: str | None, bearer: Bearer = Depends()):
+    from_user_id = bearer.id if id is None else ensure(id)
     user = bearer.user
     with Session(engine) as session:
         return [
